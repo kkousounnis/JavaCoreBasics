b14d6e083e780690067bcfc2e37df7acee81fae0
Konstantinos <kwstas654321@gmail.com>
2021-02-02 14:10:49 +0200

    Main
    Output example of all colors and shapes

--- src/FactoryDesignPattern/src/main/FactoryDesignPattern.java
+++ src/FactoryDesignPattern/src/main/FactoryDesignPattern.java
@@ -5,7 +5,9 @@
  */
 package main;
 
-import factory.ShapeFactory;
+import factory.AbstractFactory;
+import factory.FactoryProducer;
+import interfaces.Color;
 import interfaces.Shape;
 
 /**
@@ -18,16 +20,31 @@ public class FactoryDesignPattern {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ShapeFactory shapeFactory = new ShapeFactory();
 
-        Shape shape1 = shapeFactory.getShape("CIRCLE");
+        //get shape factory
+        AbstractFactory shapeFactory = FactoryProducer.getFactory("SHAPE");
+        
+        //get an object of Shape Circle
+        Shape shape1 = shapeFactory.getShape("CIRCLE");        
         shape1.draw();
-
+        
         Shape shape2 = shapeFactory.getShape("RECTANGLE");
         shape2.draw();
-
+        
         Shape shape3 = shapeFactory.getShape("SQUARE");
         shape3.draw();
+        
+        AbstractFactory colorFactory = FactoryProducer.getFactory("COLOR");
+        
+        //get an object of Color
+        Color color1 = colorFactory.getColor("RED");        
+        color1.fill();
+        
+        Color color2 = colorFactory.getColor("GREEN");
+        color2.fill();
+        
+        Color color3 = colorFactory.getColor("BLUE");
+        color3.fill();
     }
 
 }
